f0659223d469d13cefefcd44d2f9724c3c8e037f
Shepardeon <thomas.demaen@life-sensation.fr>
2025-12-25 15:44:39 +0100

    default world with no update

--- Sources/engine/physics/World.hx
+++ Sources/engine/physics/World.hx
@@ -1,3 +1,25 @@
 package engine.physics;
 
-class World {}
+class World {
+	public var xGravity:Float;
+	public var yGravity:Float;
+	public var airFriction:Float;
+	public var groundFriction:Float;
+
+	private var _bodies:Array<Body>;
+
+	public function new(xGravity:Float = 0, yGravity:Float = 0, airFriction:Float = 0.1, groundFriction:Float = 0.2) {
+		this.xGravity = xGravity;
+		this.yGravity = yGravity;
+		this.airFriction = airFriction;
+		this.groundFriction = groundFriction;
+
+		_bodies = [];
+	}
+
+	public function add(body:Body) {
+		_bodies.push(body);
+	}
+
+	public function update(dt) {}
+}
